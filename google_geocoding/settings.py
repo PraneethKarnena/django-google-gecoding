ee5bd2adc6120b2a76d44a2634ab4ac2cb0a3211
Praneeth Karnena <praneeth.codes@gmail.com>
2019-08-06 09:15:42 +0530

    configuring media files

--- google_geocoding/settings.py
+++ google_geocoding/settings.py
@@ -122,13 +122,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
